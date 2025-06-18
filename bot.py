fbacc1fb5eddf0f76b0f58662e8208398489b11d
mirjalol2008 <147647621+mirjalol2008@users.noreply.github.com>
2025-06-18 06:52:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,39 @@
 import os
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import F
+from aiohttp import web
 
+# Loggingni sozlash
 logging.basicConfig(level=logging.INFO)
 
+# Bot tokenini muhit o'zgaruvchisidan olish
 API_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-PORT = int(os.environ.get("PORT", 10000))
 
+# Bot va dispatcher yaratish
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+# /start komandasi uchun handler
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Salom! Bu mening Telegram botim.")
 
+# Har qanday xabarni qaytarish uchun handler
 @dp.message_handler()
 async def echo_message(message: types.Message):
     await message.answer(message.text)
 
+# Web server va Telegram botini ishga tushirish
 if __name__ == '__main__':
-    from aiohttp import web
-
     app = web.Application()
-    dp.setup_acls(app, bot)
-    web.run_app(app, port=PORT)
+    
+    # Samarali javoblar uchun routelar qo'shish
+    async def hello(request):
+        return web.Response(text="Bot online!")
+
+    app.router.add_get('/', hello)
+
+    # Web app ishga tushadigan port
+    port = int(os.environ.get("PORT", 10000))
+    web.run_app(app, port=port)
